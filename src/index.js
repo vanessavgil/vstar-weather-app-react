7086af0bbec87b92478aafbf048951b406ad6e8d
v. v. gil <133064860+vanessavgil@users.noreply.github.com>
2023-08-05 18:57:22 -0400

    Update index.js
    included directives regarding strict mode and create root...deleted extraneous materials

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import WeatherSrc from "./WeatherSrc";
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSrc />
-
-      <small>
-        <a href="https://github.com/vanessavgil/vstar-weather-app-react">
-          open source code
-        </a>
-        by vanessa gil
-      </small>
-    </div>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
